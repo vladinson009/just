4fc6008b9bca4e3154eda3e444d064ccbc8b0e66
vladinSon <vladinson009@icloud.com>
2024-07-06 01:47:49 +0200

    #

--- app.js
+++ app.js
@@ -3,29 +3,36 @@ form.addEventListener('submit', onClick);
 
 async function onClick(e) {
   e.preventDefault();
-  const [name, age, gender] = Array.from(form).value;
+  const [name, age, gender] = Array.from(form);
   const data = {
-    name: name,
-    age: age,
-    gender: gender,
+    name: name.value,
+    age: age.value,
+    gender: gender.value,
   };
   form.clear();
   return await postData(data);
 }
 
 async function postData(data) {
-  const url = 'https://parseapi.back4app.com/classes/people';
-  const options = {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Parse-Application-Id': 'au5ZfMxfafCmKs574dAyu0QTnNM1uOEvT9bTr4US',
-      'X-Parse-REST-API-Key': 'ZnXSOEo9LBw2GK4nEUwRRwkSb7zryrwlyEidB8VA',
-      body: JSON.stringify(data),
-    },
-  };
-  const response = await fetch(url, options);
-  const result = await response.json();
+  try {
+    const url = 'https://parseapi.back4app.com/classes/people';
+    const options = {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Parse-Application-Id': 'au5ZfMxfafCmKs574dAyu0QTnNM1uOEvT9bTr4US',
+        'X-Parse-REST-API-Key': 'ZnXSOEo9LBw2GK4nEUwRRwkSb7zryrwlyEidB8VA',
+        body: JSON.stringify(data),
+      },
+    };
+    const response = await fetch(url, options);
+    if (response.ok != true) {
+      throw new Error(response.statusText);
+    }
+    const result = await response.json();
 
-  return result;
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
