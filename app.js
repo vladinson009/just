eb117fda2bc2b0123738631ed2bd4a1839f83ada
vladinSon <vladinson009@icloud.com>
2024-07-06 02:53:49 +0200

    #

--- app.js
+++ app.js
@@ -6,15 +6,6 @@ form.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
-  for (let i = 0; i < arr.length - 1; i++) {
-    let current = arr[i];
-    let text = '';
-    for (let each of current) {
-      text += each + ' ';
-    }
-    const article = document.createElement('article');
-    article.textContent = text.trim();
-    div.appendChild(article);
-  }
+
   form.reset();
 }
