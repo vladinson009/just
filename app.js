5ca77c8625779f2c99741a9051f6fe9013377173
vladinSon <vladinson009@icloud.com>
2024-07-06 02:39:20 +0200

    #

--- app.js
+++ app.js
@@ -1,70 +1,7 @@
-let form = document.querySelector('#form');
-form.addEventListener('submit', onClick);
-async () => {
-  const data = await getData();
-  const div = document.getElementById('div');
-  data.forEach((e) => {
-    div.appendChild(e);
-  });
-};
+const form = document.querySelector('#form');
+const div = document.getElementById('div');
+const arr = Array.from(form);
 
-async function onClick(e) {
-  e.preventDefault();
-  const [name, age, gender] = Array.from(form);
-  const data = {
-    name: name.value,
-    age: age.value,
-    gender: gender.value,
-  };
-  name.value = '';
-  age.value = '';
-  gender.value = '';
-  return await postData(data);
-}
-
-async function postData(data) {
-  try {
-    const url = 'https://parseapi.back4app.com/classes/people';
-    const options = {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Parse-Application-Id': 'au5ZfMxfafCmKs574dAyu0QTnNM1uOEvT9bTr4US',
-        'X-Parse-REST-API-Key': 'ZnXSOEo9LBw2GK4nEUwRRwkSb7zryrwlyEidB8VA',
-        body: JSON.stringify(data),
-      },
-    };
-    const response = await fetch(url, options);
-    if (response.ok != true) {
-      throw new Error(response.statusText);
-    }
-    const result = await response.json();
-
-    return result;
-  } catch (error) {
-    throw new Error(error);
-  }
-}
-
-async function getData() {
-  try {
-    const url = 'https://parseapi.back4app.com/classes/people';
-    const options = {
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Parse-Application-Id': 'au5ZfMxfafCmKs574dAyu0QTnNM1uOEvT9bTr4US',
-        'X-Parse-REST-API-Key': 'ZnXSOEo9LBw2GK4nEUwRRwkSb7zryrwlyEidB8VA',
-      },
-    };
-    const response = await fetch(url, options);
-    if (response.ok != true) {
-      throw new Error(response.statusText);
-    }
-    const result = await response.json();
-
-    return result;
-  } catch (error) {
-    throw new Error(error);
-  }
+for (let i = 0; i < arr.length - 2; i++) {
+  div.appendChild(arr[i]);
 }
