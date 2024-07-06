8b5803605b006f00b3c17132acdb3f4a79e7123a
vladinSon <vladinson009@icloud.com>
2024-07-06 02:43:54 +0200

    #

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const arr = Array.from(form);
 
 form.addEventListener('submit', onSubmit);
 
-function onSubmit() {
+function onSubmit(e) {
+  e.preventDefault();
   for (let i = 0; i < arr.length - 2; i++) {
     const article = document.createElement('article');
     article.textContent = arr[i];
